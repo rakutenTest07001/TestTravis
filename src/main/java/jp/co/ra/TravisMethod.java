a6e90a36e99709f1b72b34739ae004d35b6bbbf1
rakutenTest07001 <xiaobin.chen@rakuten.com>
2016-07-05 19:55:00 +0800

    add more method

--- src/main/java/jp/co/ra/TravisMethod.java
+++ src/main/java/jp/co/ra/TravisMethod.java
@@ -1,9 +1,15 @@
 package jp.co.ra;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class TravisMethod {
 	
 	public int add(int a, int b){
 		return a+b;
 	}
 	
+	public List<Integer> greatThan10(List<Integer> numbers){
+		return numbers.stream().filter(x->x > 10).collect(Collectors.toList());
+	}
 }
--- src/test/java/jp/co/ra/TravisMethodTest.java
+++ src/test/java/jp/co/ra/TravisMethodTest.java
@@ -1,5 +1,8 @@
 package jp.co.ra;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Spy;
@@ -17,5 +20,14 @@ public class TravisMethodTest {
 	public void testAdd(){
 		int actual = travisMethod.add(1, 2);
 		Assert.assertEquals(3, actual);
+	}
+	
+	@Test
+	public void testGreatThan10(){
+		List<Integer> test = new ArrayList<>();
+		test.add(20);
+		test.add(7);
+		List<Integer> actual = travisMethod.greatThan10(test);
+		Assert.assertEquals(1, actual.size());
 	}
 }
